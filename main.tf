7ec389843d9587441de882ff670e03d2bb364e64
akashvarshney2023 <akashvarshney2023@outlook.com>
2023-05-24 02:52:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,18 @@ module "rg" {
   rg      = var.rg
 
 }
+
+module "resource_group1_test" {
+  source  = "git::https://github.com/akashvarshney2023/terraform-module-rg.git?ref=1.0.0"
+  generic = var.generic
+  rg      = var.rg
+
+}
+
+
+module "resource_group2_test" {
+  source  = "git::https://github.com/akashvarshney2023/terraform-module-rg.git?ref=1.0.0"
+  generic = var.generic
+  rg      = var.rg
+
+}
